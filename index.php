24477fcd8f6216f528cf2beba7d5e92ed9ad0dcb
jordanrchrd <jordan.rchrd@eduge.ch>
2022-09-30 10:59:51 +0200

    vue accueil / geg / index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!--
+  Auteur  : Pedro Carneiro - Jordan Richard - Céline Erni - Achraf Zamader -Yvan Lüthi
+  Date    : 30.09.2022
+  Version : V1
+  Titre   : Projet Geggui - M426
+-->
+<?php
+
+header("Location:vue/accueil.php");
+
+?>
